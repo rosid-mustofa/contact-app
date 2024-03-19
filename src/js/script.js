95328b5252208bd19418cd9f139fe2c32c9945ed
rosid-codelab <rosid@poltektegal.ac.id>
2024-03-19 19:39:30 +0700

    local storage contact-app

--- src/js/script.js
+++ src/js/script.js
@@ -1,113 +1,116 @@
-// start: Sidebar
-const sidebarToggle = document.querySelector(".sidebar-toggle");
-const sidebarOverlay = document.querySelector(".sidebar-overlay");
-const sidebarMenu = document.querySelector(".sidebar-menu");
-const main = document.querySelector(".main");
-if (window.innerWidth < 768) {
-  main.classList.toggle("active");
-  sidebarOverlay.classList.toggle("hidden");
-  sidebarMenu.classList.toggle("-translate-x-full");
-}
-sidebarToggle.addEventListener("click", function (e) {
-  e.preventDefault();
-  main.classList.toggle("active");
-  sidebarOverlay.classList.toggle("hidden");
-  sidebarMenu.classList.toggle("-translate-x-full");
-});
-sidebarOverlay.addEventListener("click", function (e) {
-  e.preventDefault();
-  main.classList.add("active");
-  sidebarOverlay.classList.add("hidden");
-  sidebarMenu.classList.add("-translate-x-full");
-});
-document.querySelectorAll(".sidebar-dropdown-toggle").forEach(function (item) {
-  item.addEventListener("click", function (e) {
-    e.preventDefault();
-    const parent = item.closest(".group");
-    if (parent.classList.contains("selected")) {
-      parent.classList.remove("selected");
-    } else {
-      document
-        .querySelectorAll(".sidebar-dropdown-toggle")
-        .forEach(function (i) {
-          i.closest(".group").classList.remove("selected");
-        });
-      parent.classList.add("selected");
-    }
-  });
-});
-// end: Sidebar
-
-// start: Popper
-const popperInstance = {};
-document.querySelectorAll(".dropdown").forEach(function (item, index) {
-  const popperId = "popper-" + index;
-  const toggle = item.querySelector(".dropdown-toggle");
-  const menu = item.querySelector(".dropdown-menu");
-  menu.dataset.popperId = popperId;
-  popperInstance[popperId] = Popper.createPopper(toggle, menu, {
-    modifiers: [
-      {
-        name: "offset",
-        options: {
-          offset: [0, 8],
-        },
-      },
-      {
-        name: "preventOverflow",
-        options: {
-          padding: 24,
-        },
-      },
-    ],
-    placement: "bottom-end",
-  });
-});
-document.addEventListener("click", function (e) {
-  const toggle = e.target.closest(".dropdown-toggle");
-  const menu = e.target.closest(".dropdown-menu");
-  if (toggle) {
-    const menuEl = toggle.closest(".dropdown").querySelector(".dropdown-menu");
-    const popperId = menuEl.dataset.popperId;
-    if (menuEl.classList.contains("hidden")) {
-      hideDropdown();
-      menuEl.classList.remove("hidden");
-      showPopper(popperId);
-    } else {
-      menuEl.classList.add("hidden");
-      hidePopper(popperId);
-    }
-  } else if (!menu) {
-    hideDropdown();
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var address = document.getElementById("address").value;
+  if (name == "") {
+    alert("name is Required");
+    return false;
   }
-});
+  if (phone == "") {
+    alert("phone is required");
+    return false;
+  }
+  if (email == "") {
+    alert("email is required");
+    return false;
+  } else if (!email.includes("@")) {
+    alert("invalid email address");
+    return false;
+  }
+  return true;
+}
+function showData() {
+  var contactList;
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+  var html = "";
+  contactList.forEach(function (element, index) {
+    html += "<tr>";
+    html += "<td>" + element.name + "</td>";
+    html += "<td>" + element.phone + "</td>";
+    html += "<td>" + element.email + "</td>";
+    html += "<td>" + element.address + "</td>";
+    html +=
+      '<td><button onclick="deleteData(' +
+      index +
+      ')" class ="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md focus:outline-none">Delete</button><button onclick="updateData(' +
+      index +
+      ')" class = "bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none pl-3"> Edit</button></td>';
+    html += "</tr>";
+  });
+  document.querySelector("#crudTable tbody").innerHTML = html;
+}
+document.onload = showData();
+function AddData() {
+  if (validateForm() == true) {
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
 
-function hideDropdown() {
-  document.querySelectorAll(".dropdown-menu").forEach(function (item) {
-    item.classList.add("hidden");
-  });
+    var contactList;
+    if (localStorage.getItem("contactList") == null) {
+      contactList = [];
+    } else {
+      contactList = JSON.parse(localStorage.getItem("contactList"));
+    }
+    contactList.push({
+      name: name,
+      phone: phone,
+      email: email,
+      address: address,
+    });
+    localStorage.setItem("contactList", JSON.stringify(contactList));
+    showData;
+    document.getElementById("name").value = "";
+    document.getElementById("phone").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("address").value = "";
+  }
 }
-function showPopper(popperId) {
-  popperInstance[popperId].setOptions(function (options) {
-    return {
-      ...options,
-      modifiers: [
-        ...options.modifiers,
-        { name: "eventListeners", enabled: true },
-      ],
-    };
-  });
-  popperInstance[popperId].update();
+function deleteData(index) {
+  var contactList;
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+  contactList.splice(index, 1);
+  localStorage.setItem("contactList", JSON.stringify(contactList));
+  showData();
 }
-function hidePopper(popperId) {
-  popperInstance[popperId].setOptions(function (options) {
-    return {
-      ...options,
-      modifiers: [
-        ...options.modifiers,
-        { name: "eventListeners", enabled: false },
-      ],
-    };
-  });
+function updateData(index) {
+  document.getElementById("submit").style.display = "none";
+  document.getElementById("update").style.display = "block";
+  var contactList;
+  if (localStorage.getItem("contactList") == null) {
+    contactList = [];
+  } else {
+    contactList = JSON.parse(localStorage.getItem("contactList"));
+  }
+  document.getElementById("name").value = contactList[index].name;
+  document.getElementById("phone").value = contactList[index].phone;
+  document.getElementById("email").value = contactList[index].email;
+  document.getElementById("address").value = contactList[index].address;
+
+  document.querySelector("#Update").onclick = function () {
+    if (validateForm() == true) {
+      contactList[index].name = document.getElementById("name").value;
+      contactList[index].phone = document.getElementById("phone").value;
+      contactList[index].email = document.getElementById("email").value;
+      contactList[index].address = document.getElementById("address").value;
+      localStorage.setItem("contactList", JSON.stringify(contactList));
+      showData();
+      document.getElementById("name").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("address").value = "";
+      document.getElementById("Submit").style.display = "block";
+      document.getElementById("Update").style.display = "none";
+    }
+  };
 }
-// end: Popper
